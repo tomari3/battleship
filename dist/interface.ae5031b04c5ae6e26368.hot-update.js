38a55d4b8ebe90434242be539cf4a9b5450fa944
Tom Ari <freak.tom3@gmail.com>
2022-03-19 18:30:49 +0200

    center boards, flex-flow

---
+++ dist/interface.ae5031b04c5ae6e26368.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatebattleship"]("interface",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("094162ff6e46c02d1cba")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW50ZXJmYWNlLmFlNTAzMWIwNGM1YWU2ZTI2MzY4LmhvdC11cGRhdGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7VUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2JhdHRsZXNoaXAvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIl9fd2VicGFja19yZXF1aXJlX18uaCA9ICgpID0+IChcIjA5NDE2MmZmNmU0NmMwMmQxY2JhXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
