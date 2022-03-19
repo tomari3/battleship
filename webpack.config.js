744dbd0d53f898e930b0f097683bce35d3d9183d
Tom Ari <freak.tom3@gmail.com>
2022-03-19 18:30:49 +0200

    webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "./src/javascript/index.js"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Battleship",
+      template: path.resolve(__dirname, "./src/html/template.html"),
+      filename: "index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: "asset/resource" },
+      { test: /\.(scss|css)$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+};
